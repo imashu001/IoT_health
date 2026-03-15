090c155dc597eb9c624b0a06257eb81ba60d494f
Ashutosh Kumar <imashu001@gmail.com>
2026-03-15 12:38:49 +0530

    version 2: Added the mongo db connection

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from database import db
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI"}
+
+@app.get("/db-test")
+async def test_db():
+    collections = await db.list_collection_names()
+    return {"collections": collections}
